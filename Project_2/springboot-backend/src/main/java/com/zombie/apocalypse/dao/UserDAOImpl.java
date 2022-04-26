577e04c47a92c64bbb5f9cb51d8236981f290c52
EricVara <vara.ada91@gmail.com>
2022-04-25 21:35:52 -0700

    began building layers

--- Project_2/springboot-backend/src/main/java/com/zombie/apocalypse/dao/UserDAOImpl.java
+++ Project_2/springboot-backend/src/main/java/com/zombie/apocalypse/dao/UserDAOImpl.java
@@ -1,5 +1,7 @@
 package com.zombie.apocalypse.dao;
 import com.zombie.apocalypse.data.User;
+
+import java.sql.Connection;
 import java.util.*;
 
 public class UserDAOImpl implements UserDAO {
@@ -7,8 +9,6 @@ public class UserDAOImpl implements UserDAO {
     @Override
     public List<User> getUserWithId(Long id) {
         // JDBC - Java Database Connection API
-
-
         return null;
     }
 }
