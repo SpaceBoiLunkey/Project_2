ae1a84df4ce81ad830047152c8db7b20d68851c4
EricVara <vara.ada91@gmail.com>
2022-04-29 01:16:06 -0700

    added cntroller

--- Project_2/springboot-backend/src/main/java/com/zombie/apocalypse/App.java
+++ Project_2/springboot-backend/src/main/java/com/zombie/apocalypse/App.java
@@ -1,20 +1,12 @@
 package com.zombie.apocalypse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class SpringbootApp implements CommandLineRunner {
-    @Autowired
-    JdbcTemplate jdbcTemplate;
-
+public class App {
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootApp.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
+        SpringApplication.run(App.class, args);
     }
 }
