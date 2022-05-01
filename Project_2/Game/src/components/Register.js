344a4ba46b7ee74ed20e9205965a7ead6123e2ea
donaldlopez82 <donaldlopez@donaldlopezart.com>
2022-04-30 23:22:18 -0700

    Working on Store

--- Project_2/Game/src/components/Register.js
+++ Project_2/Game/src/components/Register.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-
 export default function Register() {
   return (
     <form  className="flex justify-content max-w-xs mx-auto mt-5 "  >
@@ -17,18 +16,18 @@ export default function Register() {
    <div className="col text-center "  >
    <span class="bg-dark d-inline-block p-5 mr-3 rounded" >
    <div className="">
-       <label><h5>Email</h5></label>
+       <label><h5>Enter Email</h5></label>
        <input type="email" className="form-control" placeholder="Enter Email" />
    </div>
    <br></br>
    <div className="">
-   <label><h5>Password</h5></label>
+   <label><h5>Create Password</h5></label>
        <input type="password"  className="form-control" placeholder="Enter password" />
    </div>
 
 <br></br>
 
-<Link to="/Login" type="submit" className="btn btn-success btn-lg btn-block"><h5>Login</h5></Link>
+<Link to="/Login" type="submit" className="btn btn-success btn-lg btn-block"><h5>Submit</h5></Link>
 </span>
    </div>
    <br></br><br></br><br></br>
